b37bd85f57afea1534850beaa81a8c50c2fb3372
Rolf Lawrenz <rolf.lawrenz@gmail.com>
2013-08-08 23:05:43 -0600

    Initial Commit

---
+++ src/com/games/candyland/CandyLandGame.java
@@ -0,0 +1,13 @@
+package com.games.candyland;
+
+import com.games.candyland.model.GameBoard;
+
+public class CandyLandGame {
+
+	public static void main(String[] args) {
+		GameBoard board = new GameBoard();
+
+		board.play();
+	}
+
+}
---
+++ src/com/games/candyland/model/GameBoard.java
@@ -0,0 +1,8 @@
+package com.games.candyland.model;
+
+public class GameBoard {
+
+	public void play() {
+		System.out.println("Lets play CandyLand");
+	}
+}
